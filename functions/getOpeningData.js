01534feaf98377972157830c5970b114e907213b
Maarten Lagerweij <maarten.lagerweij1@gmail.com>
2023-10-01 10:35:27 +0200

    Initiate project

---
+++ functions/getOpeningData.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const axios = require('axios');
+require('dotenv').config();
+
+const API_KEY = process.env.API_KEY;
+const DETAILS_URL = 'https://maps.googleapis.com/maps/api/place/details/json';
+const FILE_PATH = __dirname+'/../data/sauna-openingstijden.json';
+
+const saunas = JSON.parse(fs.readFileSync(FILE_PATH, 'utf8'));
+
+async function getOpeningHours(place_id){
+    try {
+       const response = await axios.get(DETAILS_URL, {
+        params: { 
+            key: API_KEY,
+            place_id: place_id,
+            fields: 'opening_hours'
+        }
+       })
+       if(response.data && response.data.result && response.data.result.opening_hours) return response.data.result.opening_hours.weekday_text;
+       return null;
+    }catch(err){
+        console.error('Error with requestion Places API Opening time Data: ', err)
+        return null;
+    }
+}
+
+async function updateOpeninghours(){
+    for(let sauna of saunas){
+        const saunaName = Object.keys(sauna)[0];
+        if(!Object.values(sauna)[0]['openingstijden']) {
+            const hours = await getOpeningHours(sauna[saunaName].PLACES_ID);
+            hours ? sauna[saunaName].openingstijden = hours : sauna[saunaName].openingstijden = 'No times found'
+        };
+    }
+    fs.writeFileSync(FILE_PATH, JSON.stringify(saunas, null, 2));
+}
+
+updateOpeninghours();
---
+++ functions/getPlaceIDs.js
@@ -0,0 +1,53 @@
+const axios = require('axios');
+const fs = require("fs");
+require('dotenv').config();
+
+const API_KEY = process.env.API_KEY;
+const BASE_URL = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json';
+const FILE_PATH = __dirname+'/../data/sauna-openingstijden.json';
+
+async function searchPlace(query) {
+    try {
+        const response = await axios.get(BASE_URL, {
+            params: {
+                key: API_KEY,
+                input: query,
+                inputtype: 'textquery',
+                fields: 'place_id'
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching data:", error.response.data);
+        throw error;
+    }
+}
+
+// Laad de sauna's uit het JSON-bestand
+const saunas = JSON.parse(fs.readFileSync(FILE_PATH, 'utf8'));
+
+// Loop over de sauna's en haal de PLACES_ID op voor elk van hen
+async function fetchAndUpdatePlacesIds() {
+    for (let saunaObj of saunas) {
+        const saunaName = Object.keys(saunaObj)[0];
+        
+        // Alleen de PLACES_ID ophalen als het nog niet bestaat voor de sauna
+        if (!saunaObj[saunaName].PLACES_ID) {
+            try {
+                const data = await searchPlace(saunaName);
+                const placeId = data.candidates && data.candidates[0] ? data.candidates[0].place_id : null;
+
+                if (placeId) {
+                    saunaObj[saunaName].PLACES_ID = placeId;
+                }
+            } catch (err) {
+                console.error(`Error fetching PLACES_ID for ${saunaName}:`, err.message);
+            }
+        }
+    }
+
+    // Update het JSON-bestand met de nieuwe PLACES_IDs
+    fs.writeFileSync(FILE_PATH, JSON.stringify(saunas, null, 4));
+}
+
+fetchAndUpdatePlacesIds();
